82254e9c33b6245f402b93fc648b7749194c9f17
Anchit Gangrade <anchit987@gmail.com>
2021-03-18 15:15:40 +0530

    add dependecy injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:movie_app/data/core/api_client.dart';
-import 'package:movie_app/data/data_sources/movie_remote_data_source..dart';
-import 'package:movie_app/data/repositories/movie_repository_impl.dart';
-import 'package:movie_app/domain/entities/app_error.dart';
-import 'package:movie_app/domain/entities/movie_entity.dart';
-import 'package:movie_app/domain/entities/no_params.dart';
-import 'package:movie_app/domain/repositories/movie_repository.dart';
-import 'package:movie_app/domain/usecases/get_trending.dart';
+import 'package:pedantic/pedantic.dart';
+
+import 'di/get_in.dart' as getIt;
+import 'domain/entities/app_error.dart';
+import 'domain/entities/movie_entity.dart';
+import 'domain/entities/no_params.dart';
+import 'domain/usecases/get_trending.dart';
 
 Future<void> main() async {
-  ApiClient apiClient = ApiClient(Client());
-  MovieRemoteDataSource dataSource = MovieRemoteDataSourceImpl(apiClient);
-  MovieRepository movieRepository = MovieRepositoryImp(dataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>> eitherResponse =
       await getTrending(NoParams());
   eitherResponse.fold((l) {
